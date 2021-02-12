a2300fc733784a06d8e9e242ebf11f95fb9e1837
Mobarak Ali <mobarak.ali@outlook.com>
2021-02-12 20:28:32 +0600

    Lesson04 completed

---
+++ js/lesson04.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 50;
+let club = "Tottenham Hostspur";
+const isFamous = false;
+let famous;
+age = 99;
+console.log(age);
---
+++ lesson04.ts
@@ -0,0 +1,8 @@
+let age:number = 50;
+let club:string = "Tottenham Hostspur";
+const isFamous:boolean = false;
+let famous:boolean;
+
+age = 99;
+
+console.log(age);
